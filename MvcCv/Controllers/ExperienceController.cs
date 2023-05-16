f6cf2c808863310ccc9e51bdcf2ec9f159991d92
Ufuk Çelik <ufukcelik.dev@gmail.com>
2023-05-16 20:13:54 +0300

    Resume

---
+++ MvcCv/Controllers/ExperienceController.cs
@@ -0,0 +1,58 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceRepository repo = new ExperienceRepository();
+
+        public ActionResult Index()
+        {
+            var values = repo.List();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddExperience()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddExperience(TblExperience p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveExperience(int id)
+        {
+            TblExperience t = repo.Find(x => x.ID == id);
+            repo.TDelete(t);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult GetExperience(int id)
+        {
+            TblExperience t = repo.Find(x => x.ID == id);
+            return View(t);
+        }
+        [HttpPost]
+        public ActionResult GetExperience(TblExperience p)
+        {
+            TblExperience t = repo.Find(x => x.ID == p.ID);
+            t.Heading = p.Heading;
+            t.subtitle = p.subtitle;
+            t.Date = p.Date;
+            t.Description = p.Description;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCv/Repositories/GenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Ajax.Utilities;
+using MvcCv.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace MvcCv.Repositories
+{
+    //T değeri göndereceğimiz sınıflar olucak.
+    //T değeri bir sınıf olması şartıyla ve newlenebilir olmalıdır.
+    public class GenericRepository<T> where T : class, new()
+    {
+        DbCvEntities db = new DbCvEntities();
+
+        public List<T> List()
+        {
+            return db.Set<T>().ToList();
+        }
+
+        public void TAdd(T p)
+        {
+            db.Set<T>().Add(p);
+            db.SaveChanges();
+        }
+
+        public void TDelete(T p)
+        {
+            db.Set<T>().Remove(p);
+            db.SaveChanges();
+        }
+
+        //ID'ye göre bulma işlemi
+        public T TGet(int id)
+        {
+            return db.Set<T>().Find(id);
+        }
+
+        public void TUpdate(T p)
+        {
+            db.SaveChanges();
+        }
+
+        //Bul metodu oluşturuyoruz
+        public T Find(Expression<Func<T, bool>> where)
+        {
+            return db.Set<T>().FirstOrDefault(where);
+        }
+    }
+}
